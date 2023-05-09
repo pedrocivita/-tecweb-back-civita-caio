d8e21df8a5df0d5bbe38d3a2382502b5a165889c
CaioBoa <caioob@al.insper.edu.br>
2023-05-09 14:17:59 -0300

    deploy 1

--- tecweb/settings.py
+++ tecweb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-o#tw*x0b@q^ceeey!l^rh&2%wx5rab4(%(lt0i0-zw!hm-=$w8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -55,6 +57,25 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'tecweb.urls'
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://postgres:4HYI9wsuis3TrF4WrnvZ@containers-us-west-17.railway.app:7687/railway',
+        conn_max_age=600,
+        ssl_require=not DEBUG
+    )
+}
+
+WSGI_APPLICATION = 'tecweb.wsgi.application'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+# Resto do código...
 
 TEMPLATES = [
     {
@@ -78,19 +99,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
 ]
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# Resto do código...
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-# Resto do código...
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -127,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
